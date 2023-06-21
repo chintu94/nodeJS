b457d61bc44045eeeb34feffcaa0710d22b6880f
chintu <chintu.sahu@ibm.com>
2023-06-21 08:29:48 +0530

    final commit from mac

--- test.js
+++ test.js
@@ -37,5 +37,53 @@ var arr = [
   "apple",
   ["banana", "strawberry", "apple", 25]
 ];
-console.log(numberOfItems(arr, 25));
-console.log(numberOfItems(arr, "apple"));
+// console.log(numberOfItems(arr, 25));
+// console.log(numberOfItems(arr, "apple"));
+
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+//! Write a function:
+
+// function solution(A);
+
+// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+// Given A = [1, 2, 3], the function should return 4.
+
+// Given A = [−1, −3], the function should return 1.
+
+// Write an efficient algorithm for the following assumptions:
+
+// N is an integer within the range [1..100,000];
+// each element of array A is an integer within the range [−1,000,000..1,000,000].
+function solution(A) {
+  // Implement your solution here
+  const sortedArray = A.sort((a,b) => a-b);
+  let ans = undefined;
+  let largestNum = sortedArray[sortedArray.length - 1];
+  for (let i = largestNum; i> 0; i--){
+      if (!A.includes(i)){
+          ans = i;
+          break;
+      }
+  }
+  if (!ans){
+      if (largestNum < 0){
+          ans = 1
+      }else{
+          ans = largestNum + 1
+      }
+  }
+  return ans;
+}
+
+/**
+
+
+
+
+
+ */
